57a7f970cc59a59e1ff635438ed8369ef34a03a9
Vinicius de Lacerda <vlacerda@gmail.com>
2012-04-13 13:34:12 -0300

    Mudança de nome na view de usuarios

--- modules/admin/views/admin/user/home.php
+++ modules/admin/views/admin/user/home.php
@@ -1,2 +1,2 @@
-<h1>Seja bem vindo ao Sistema de Gerenciamento de conteúdo, do website Milanez Arquitetos</h1>
+<h1>Seja bem vindo ao Sistema de Gerenciamento de conteúdo da Urango Studio</h1>
 <p>Use o menu acima para navegar no CMS</p>
